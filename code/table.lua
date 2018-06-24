69c0cff20506b1d11634c5520f128c69cba65904
大菠萝 <2062152083@qq.com>
2018-06-24 23:14:21 +0800

    Update table.lua

--- code/table.lua
+++ code/table.lua
@@ -2,3 +2,81 @@
 处理table的一些函数
 ]]
 
+--dump 打印出所有表结构
+function dump(value, desciption, nesting)
+    if type(nesting) ~= "number" then nesting = 3 end
+
+    local lookupTable = {}
+    local result = {}
+
+    local traceback = string.split(debug.traceback("", 2), "\n")
+    print("dump from: " .. string.trim(traceback[3]))
+
+    local function dump_(value, desciption, indent, nest, keylen)
+        desciption = desciption or "<var>"
+        local spc = ""
+        if type(keylen) == "number" then
+            spc = string.rep(" ", keylen - string.len(dump_value_(desciption)))
+        end
+        if type(value) ~= "table" then
+            result[#result +1 ] = string.format("%s%s%s = %s", indent, dump_value_(desciption), spc, dump_value_(value))
+        elseif lookupTable[tostring(value)] then
+            result[#result +1 ] = string.format("%s%s%s = *REF*", indent, dump_value_(desciption), spc)
+        else
+            lookupTable[tostring(value)] = true
+            if nest > nesting then
+                result[#result +1 ] = string.format("%s%s = *MAX NESTING*", indent, dump_value_(desciption))
+            else
+                result[#result +1 ] = string.format("%s%s = {", indent, dump_value_(desciption))
+                local indent2 = indent.."    "
+                local keys = {}
+                local keylen = 0
+                local values = {}
+                for k, v in pairs(value) do
+                    keys[#keys + 1] = k
+                    local vk = dump_value_(k)
+                    local vkl = string.len(vk)
+                    if vkl > keylen then keylen = vkl end
+                    values[k] = v
+                end
+                table.sort(keys, function(a, b)
+                    if type(a) == "number" and type(b) == "number" then
+                        return a < b
+                    else
+                        return tostring(a) < tostring(b)
+                    end
+                end)
+                for i, k in ipairs(keys) do
+                    dump_(values[k], k, indent2, nest + 1, keylen)
+                end
+                result[#result +1] = string.format("%s}", indent)
+            end
+        end
+    end
+    dump_(value, desciption, "- ", 1)
+
+    for i, line in ipairs(result) do
+        print(line)
+    end
+    return result
+end
+
+--clone table
+local tableClone = function(object)--clone函数
+    local lookup_table = {}--新建table用于记录
+    local function _copy(object)--_copy(object)函数用于实现复制
+        if type(object) ~= "table" then
+            return object   ---如果内容不是table 直接返回object(例如如果是数字\字符串直接返回该数字\该字符串)
+        elseif lookup_table[object] then
+            return lookup_table[object]--这里是用于递归滴时候的,如果这个table已经复制过了,就直接返回
+        end
+        local new_table = {}
+        lookup_table[object] = new_table--新建new_table记录需要复制的二级子表,并放到lookup_table[object]中.
+        for key, value in pairs(object) do
+            new_table[_copy(key)] = _copy(value)--遍历object和递归_copy(value)把每一个表中的数据都复制出来
+        end
+        return setmetatable(new_table, getmetatable(object))--每一次完成遍历后,就对指定table设置metatable键值
+    end
+    return _copy(object)--返回clone出来的object表指针/地址
+end
+
